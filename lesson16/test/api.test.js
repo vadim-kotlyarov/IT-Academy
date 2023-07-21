33592f6d9ccddc78bf48e94027df00a0560a71b5
vadim kotlyarov <vadimkotlyarov1990@gmail.com>
2023-07-21 13:09:55 +0300

    fix tests

--- lesson16/test/api.test.js
+++ lesson16/test/api.test.js
@@ -3,120 +3,126 @@ const validator = require('jsonschema');
 const getBooksSchema = require('../data/fakeAPIBooks.post.v1.json');
 const getNewBooksSchema = require('../data/fakeAPIBooks.post.v2.json');
 
-describe('API tests GET', function () {
+describe('API tests GET list of books', function () {
     let response;
     beforeAll(async () => {
-        response = await axios.get('https://fakerestapi.azurewebsites.net/api/v1/Books', {
-            headers: {
-                Accept: 'application/json'
-            }
-        });
-    })
-    test('GET request should be 200', async () => {
+        response = await axios.get('https://fakerestapi.azurewebsites.net/api/v1/Books');
+    });
+    test('GET list of books status code should be 200', async () => {
         await expect(response.status).toEqual(200);
     });
-    test('GET should be valid json schema', async () => {
+    test('GET list of books response body should be with valid json schema', async () => {
         const result = await validator.validate(response.data, getBooksSchema);
         await expect(result.valid).toEqual(true);
     });
 });
 
-describe('API tests POST', function () {
+describe('API tests POST create book', function () {
     let response;
+    let id = 12;
     beforeAll(async () => {
         response = await axios.post('https://fakerestapi.azurewebsites.net/api/v1/Books', {
+            "id": id,
+            "title": "my page",
+            "description": "my description",
+            "pageCount": 150,
+            "excerpt": "nothing",
+            "publishDate": "2023-07-16T21:22:24.330Z"
+
+        }, {
             headers: {
-                Accept: 'application/json'
-            },
-            data: {
-                "id": 10,
-                "title": "my book",
-                "description": "my description",
-                "pageCount": 100,
-                "excerpt": "nothing",
-                "publishDate": "2023-07-16T21:22:24.330Z"
+                'Content-Type': 'application/json'
             }
         });
-    })
-    test('POST request should be 200', async () => {
+    });
+    afterAll(async () => {
+        await axios.delete(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`);
+    });
+    test('POST create book status code should be 200', async () => {
         await expect(response.status).toEqual(200);
     });
-    test('POST should be valid json schema', async () => {
+    test('POST create book response body should be with valid json schema', async () => {
         const result = await validator.validate(response.data, getNewBooksSchema);
         await expect(result.valid).toEqual(true);
     });
 });
 
-describe('API tests GET2', function () {
+describe('API tests GET book by id', function () {
     let response;
     let id = 12;
     beforeAll(async () => {
-        response = await axios.get(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`, {
-            headers: {
-                Accept: 'application/json'
-            }
-        });
+        response = await axios.get(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`);
     })
-    test('GET2 request should be 200', async () => {
+    test('GET book by id status code should be 200', async () => {
         await expect(response.status).toEqual(200);
     });
-    test('GET2 should be valid json schema', async () => {
+    test('GET book by id response body should be with valid json schema', async () => {
         const result = await validator.validate(response.data, getNewBooksSchema);
         await expect(result.valid).toEqual(true);
     });
 });
 
-describe('API tests PUT', function () {
-    let response;
-    let id = 12;
-    beforeAll(async () => {
-        response = await axios.put(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`, {
-            headers: {
-                Accept: 'application/json'
-            },
-            data: {
-                "id": 0,
-                "title": "string",
-                "description": "string",
-                "pageCount": 0,
-                "excerpt": "string",
-                "publishDate": "2023-07-16T21:51:08.158Z"
-            }
-        });
-    })
-    test('PUT request should be 200', async () => {
-        await expect(response.status).toEqual(200);
-    });
-    test('PUT should be valid json schema', async () => {
-        const result = await validator.validate(response, getNewBooksSchema);
-        await expect(result.valid).toEqual(true);
-    });
-});
-
-describe('API tests DELETE', function () {
+describe('API tests PUT update book', function () {
     let response;
     let id = 12;
     beforeAll(async () => {
         await axios.post('https://fakerestapi.azurewebsites.net/api/v1/Books', {
+            "id": id,
+            "title": "my page",
+            "description": "my description",
+            "pageCount": 150,
+            "excerpt": "nothing",
+            "publishDate": "2023-07-16T21:22:24.330Z"
+
+        }, {
             headers: {
-                Accept: 'application/json'
-            },
-            data: {
-                "id": 12,
-                "title": "my book",
-                "description": "my description",
-                "pageCount": 100,
-                "excerpt": "nothing",
-                "publishDate": "2023-07-16T21:22:24.330Z"
+                'Content-Type': 'application/json'
             }
         });
-        response = await axios.delete(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`, {
+        response = await axios.put(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`, {
+            "id": id,
+            "title": "my book",
+            "description": "my description",
+            "pageCount": 100,
+            "excerpt": "nothing",
+            "publishDate": "2023-07-16T21:22:24.330Z"
+        }, {
             headers: {
-                Accept: 'application/json'
+                'Content-Type': 'application/json'
             }
         });
-    })
-    test('DELETE request should be 200', async () => {
+    });
+    afterAll(async () => {
+        await axios.delete(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`);
+    });
+    test('PUT update book status code should be 200', async () => {
+        await expect(response.status).toEqual(200);
+    });
+    test('PUT update book response body should be wiht valid json schema', async () => {
+        const result = await validator.validate(response.data, getNewBooksSchema);
+        await expect(result.valid).toEqual(true);
+    });
+});
+
+describe('API tests DELETE book by id', function () {
+    let response;
+    let id = 12;
+    beforeAll(async () => {
+        await axios.post('https://fakerestapi.azurewebsites.net/api/v1/Books', {
+            "id": 12,
+            "title": "my book",
+            "description": "my description",
+            "pageCount": 100,
+            "excerpt": "nothing",
+            "publishDate": "2023-07-16T21:22:24.330Z"
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        response = await axios.delete(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`);
+    });
+    test('DELETE book by id status code should be 200', async () => {
         await expect(response.status).toEqual(200);
     });
 });
